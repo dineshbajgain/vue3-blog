7c8bd6a9ebd2ee67bbc6f5a2f026eceefd85f24b
Dinesh Bajgain <bajgaindinesh@gmail.com>
2021-10-23 22:42:20 +0545

    added condition and logic

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,12 @@
   <div class="home">
       <h1>Home</h1>
       <div v-if="error">{{ error }}</div>
-      <PostList :posts="posts" />
+      <div v-if="posts.length">
+       <PostList :posts="posts" />
+      </div>
+      <div v-else>
+        Loading ...
+      </div>
    </div>
 </template>
 <script>
