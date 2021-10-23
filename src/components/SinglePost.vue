afd5b86a908e79067b47139c3ea67e97b6abbae6
Dinesh Bajgain <bajgaindinesh@gmail.com>
2021-10-23 21:07:11 +0545

    Single Post and computed method added

--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -1,14 +1,24 @@
 <template>
   <div>
+      <h3>
       {{post.title}}
+      </h3>
+      <p>
+        {{snippet}}
+      </p>
   </div>
 </template>
 
 <script>
+import { computed } from '@vue/reactivity'
 export default {
     props:['post'],
-    setup(){
+    setup(props){
+        const snippet = computed (()=>{
+            return props.post.body.substring(0,100)+'...'
+        })
 
+        return { snippet }
     }
 }
 </script>
