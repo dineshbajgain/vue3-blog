4e9f0e470551ecb2e610fedee395ea039b427004
Dinesh Bajgain <bajgaindinesh@gmail.com>
2021-10-23 17:31:28 +0545

    ref as reactive property

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="home">
    home
+   <p>
+     My name is {{name}} and my age is {{age}}.
+    </p>
+     <button @click="handelClick">click me</button>
+   
   </div>
 </template>
 
 <script>
+import {ref} from 'vue'
 export default {
   name: 'Home',
-  components: {
-  }
+  setup(){
+    const name = ref('mario')
+    const age = ref(12)
+    const handelClick = ()=> {
+      name.value='dinesh'
+      age.value=23
+    }
+    return{name,age,handelClick}
+  },
 }
 </script>
