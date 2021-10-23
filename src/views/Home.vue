8214dbf50b802ad26f728a18996c4fa3a64f631e
Dinesh Bajgain <bajgaindinesh@gmail.com>
2021-10-23 17:54:54 +0545

    reactive variable using refs and reactive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,32 @@
 <template>
   <div class="home">
-   <h1>Home</h1>
-   <h1>Refs</h1>
-   <p>{{first.name}} - {{first.age}}</p>
-   <button @click="updateFirst">Update First</button>
+      <h1>Home</h1>
+      <h1>Refs</h1>
+      <p>{{first.name}} - {{first.age}}</p>
+      <button @click="updateFirst">Update First</button>
+      <h1>Reactive</h1>
+      <p>{{second.name}} - {{second.age}}</p>
+      <button @click="updateSecond">Update Second</button>
+
    </div>
 </template>
 
 <script>
-import {ref} from 'vue'
+import {ref, reactive} from 'vue'
 export default {
   name: 'Home',
   setup(){
-    const first = ref({name:'mario',age:30})
+    const first = ref({name:'Dinesh',age:23})
+    const second = reactive({name:'Laxman',age:28})
     const updateFirst = ()=>{
-      first.value.age = 40
+      first.value.name='Dinesh Bajgain'
+      first.value.age = 25
     }
-    return{first,updateFirst}
+    const updateSecond = ()=>{
+      second.name = 'Laxman Bajgain'
+      second.age = 40
+    }
+    return{first,second,updateFirst,updateSecond}
   },
 }
 </script>
