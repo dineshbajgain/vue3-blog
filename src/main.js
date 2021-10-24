61766299095376e95662c90605f18585c1717f4e
Dinesh Bajgain <bajgaindinesh@gmail.com>
2021-10-24 07:45:10 +0545

    tailwind initialize

--- src/main.js
+++ src/main.js
@@ -4,4 +4,5 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import './assets/tailwind.css'
 createApp(App).use(store).use(router).use(VueAxios, axios).mount('#app')
